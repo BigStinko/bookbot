c972fac8ac2b50df4f99640ba34324ad5f48e184
BigStinko <charlieyoung0807@gmail.com>
2023-12-08 20:16:31 -0600

    counts words and characters in given file

---
+++ main.py
@@ -0,0 +1,53 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text: str) -> int:
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path: str) -> str:
+    with open(path) as f:
+        return f.read()
+
+
+def get_chars_dict(text: str) -> dict:
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def chars_dict_to_sorted_list(num_chars_dict: dict) -> list:
+    sorted_list = []
+    for char in num_chars_dict:
+        sorted_list.append({"char": char, "num": num_chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def sort_on(d: dict) -> int:
+    return d["num"]
+
+
+main()
